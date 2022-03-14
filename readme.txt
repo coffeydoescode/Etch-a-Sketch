d394ed4cebaf00599e448629d9ec40462d7fb447
coffeydoescode <seansince98@gmail.com>
2022-03-14 10:56:55 -0400

    Add Function to Create Canvas
    Created a function that adds more area for the sketching canvas.

--- readme.txt
+++ readme.txt
@@ -2,6 +2,3 @@ This project is website where a user can create art with a basic Etch-a-Sketch
 
 As part of this project, I will be more detailed, clear and intentional with my git commits. I have implemented a
 commit template in order to ensure consistency.
-
-
-Hello World
--- script.js
+++ script.js
@@ -3,3 +3,12 @@ const div = document.createElement("div");
 const divContainer = document.querySelector(".container");
 
 divContainer.appendChild(div);
+
+function divBuilder(numOfDivs) {
+  for (let i = 0; i < numOfDivs ** 2; i++) {
+    const div = document.createElement("div");
+    div.className = "paper";
+    divContainer.appendChild(div);
+  }
+}
+divBuilder(16);
