5d62e05decc828835b2b687a3387fe418ce8b966
coffeydoescode <seansince98@gmail.com>
2022-03-17 12:03:13 -0400

    Add Comments, and Function to Change "Brush" Color
    Added comments for easily readable code.
    Added a function to easily change "brush" color
    Added a function to easily change grid size.

--- script.js
+++ script.js
@@ -5,45 +5,53 @@ function createRow() {
   rowBuild.className = "row";
   divContainer.append(rowBuild);
 }
+// This function builds a row inside the canvas
 
 function rowCount(numbOfRows) {
   for (let i = 0; i < numbOfRows; i++) {
     createRow();
   }
 }
+// This function builds a given amount of rows based on numOfRows specified
 
 function createColumn(selector) {
   const column = document.createElement("div");
   column.className = "column";
   selector.append(column);
 }
+// This function builds a column and appends it to the chosen selector
 
 function columnCount(numbOfColumns, currentRow) {
   for (let i = 0; i < numbOfColumns; i++) {
     createColumn(currentRow);
   }
 }
+// This function builds a given amount of columns based on numOfColums specified
 
-// function gridSize(length) {
-//   rowCount(length);
-//   columnCount(length);
-// }
-
-rowCount(10);
-
-const rowArray = Array.from(document.querySelectorAll(".row"));
-
-for (let i = 0; i < rowArray.length; i++) {
-  let currentRow = rowArray[i];
-  rowArray[i] = document.querySelector(".row");
-  columnCount(10, currentRow);
+function gridSize(length) {
+  rowCount(length);
+  const rowArray = Array.from(document.querySelectorAll(".row"));
+  for (let i = 0; i < rowArray.length; i++) {
+    let currentRow = rowArray[i];
+    rowArray[i] = document.querySelector(".row");
+    columnCount(length, currentRow);
+  }
 }
+// This function updates the grid size based on the given number
+
+gridSize(4);
 
 const columnArray = Array.from(document.querySelectorAll(".column"));
 
-for (let i = 0; i < columnArray.length; i++) {
-  let currentColumn = columnArray[i];
-  currentColumn.addEventListener("mouseenter", () => {
-    currentColumn.style.backgroundColor = "red";
-  });
+let color = "red";
+
+function colorChange(color) {
+  for (let i = 0; i < columnArray.length; i++) {
+    let currentColumn = columnArray[i];
+    currentColumn.addEventListener("mouseenter", () => {
+      currentColumn.style.backgroundColor = color;
+    });
+  }
 }
+// This function changes the background color of the grid boxes that the user "hovers"
+colorChange("red");
